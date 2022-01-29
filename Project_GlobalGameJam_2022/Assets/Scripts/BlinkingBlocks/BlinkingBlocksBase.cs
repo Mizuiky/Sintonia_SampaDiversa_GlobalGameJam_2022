e0fea56062a449064221501dfbec61a2a624668b
calvinfranca <calvin.franca@gmail.com>
2022-01-29 13:31:47 -0300

    BlinkingBlocks
    Prefab, Script and SceneCalvin

---
+++ Project_GlobalGameJam_2022/Assets/Scripts/BlinkingBlocks/BlinkingBlocksBase.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlinkingBlocksBase : MonoBehaviour
+{
+    public float timerBlink = 2f;
+    
+    //public BoxCollider2D colliderBlink;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Blink());
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        
+    }
+
+    IEnumerator Blink()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(timerBlink);
+
+            gameObject.GetComponent<BoxCollider2D>().enabled = false;
+
+            Color colorBlock = this.GetComponent<SpriteRenderer>().color;
+            colorBlock.a = 0.3f;
+            this.GetComponent<SpriteRenderer>().color = colorBlock;
+    
+            yield return new WaitForSeconds(timerBlink);
+
+            gameObject.GetComponent<BoxCollider2D>().enabled = true;
+
+            colorBlock = this.GetComponent<SpriteRenderer>().color;
+            colorBlock.a = 1f;
+            this.GetComponent<SpriteRenderer>().color = colorBlock;
+
+        }
+
+      
+
+
+    }
+}
