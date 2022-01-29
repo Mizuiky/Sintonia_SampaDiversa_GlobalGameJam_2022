a31a2e14c6a9af689b17216744c420592e0afb60
Priscila da Rocha Alves <priscila.mizuiky@gmail.com>
2022-01-29 20:18:12 -0300

    Adding BPM Manager + keyboards

---
+++ Project_GlobalGameJam_2022/Assets/Scripts/BPM/Keyboard.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Keyboard: MonoBehaviour
+{
+    #region Private Fields 
+    public PlayerType player;
+
+    public List<Key> keys;
+    #endregion
+}
---
+++ Project_GlobalGameJam_2022/Assets/Scripts/Player/PlayerType.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PlayerType
+{
+    Indian = 0,
+    Man = 1
+}
